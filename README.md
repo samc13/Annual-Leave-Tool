051bd5d7075f858d07115a32b51a955d4689ea04
SamClarke <sam.c94@hotmail.com>
2024-08-11 21:09:43 +0100

    Further basics - filter the list, extracting taken/booked leave. Begin to outline the concept of an allowance

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ Make sure it worked, try:
 ```bash
 python3 --version;
 ```
+
+You'll also need [PIP](https://pypi.org/project/pip/). 
+
+PIP is a package manager for Python packages, or modules if you like.
+
+If you've got Python3 3.4+, you should already have `pip`. 
+
+To install a module, you can run: 
+
+```bash
+python3 -m pip install
+``` 
+
+followed by the name of a module, such as `numpy`.
--- leave.py
+++ leave.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Leave:
     def __init__(self, date, booked, taken, metadata, halfday):
         self.date = date
@@ -7,6 +9,16 @@ class Leave:
         self.halfday = bool(halfday)
     def __str__(self) -> str:
         return f"{self.date} {self.booked} {self.taken} {self.metadata} {self.halfday}"
+    
+class Allowance:
+    def __init__(self, base, birthday, bonus, bought, bankHoliday) -> None:
+        self.base = int(base)
+        self.birthday = int(birthday)
+        self.bonus = int(bonus)
+        self.bought = int(bought)
+        self.bankHoliday = int(bankHoliday)
+    def total(self):
+        self.base + self.birthday + self.bonus + self.bought + self.bankHoliday
 
 leaveItems = []
 
@@ -16,3 +28,10 @@ for line in file:
     columns = line.split(',')
     leaveItems.append(Leave(columns[0], columns[1], columns[2], columns[3], columns[4]))
 file.close
+
+takenLeave = [l for l in leaveItems if l.taken == True]
+bookedLeave = [l for l in leaveItems if l.taken == False and l.booked == True]
+
+
+print("Total taken leave:  {}".format(len(takenLeave)))
+print("Total booked leave: {}".format(len(bookedLeave)))
